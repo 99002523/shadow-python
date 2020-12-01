5db0fb0492a8e63328a22e9546e24086429b2a68
99002523 <71481081+99002523@users.noreply.github.com>
2020-12-01 12:07:24 +0530

    Update multi.py

--- multi.py
+++ multi.py
@@ -3,7 +3,7 @@ import argparse
 import imutils
 import glob
 import cv2
-
+import pyautogui
 def Avg(a,b):
     c=0
     sum=[]
@@ -12,25 +12,26 @@ def Avg(a,b):
         c=int(c/2)
         sum.append(c)
     #sum[0]=sum[0]+439
-    #sum[1]=sum[1]+285
+    sum[1]=sum[1]+60
+    print(sum)
     return sum
 
 def Apr():
         # construct the argument parser and parse the arguments
-        ap = argparse.ArgumentParser()
-        ap.add_argument("-t", "--template", required=True, help="D:\main\guru.png")
-        ap.add_argument("-i", "--images", required=True,help="D:\main")
-        ap.add_argument("-v", "--visualize",help="Flag indicating whether or not to visualize each iteration")
-        args = vars(ap.parse_args())
+        #ap = argparse.ArgumentParser()
+        #ap.add_argument("-t", "--template", required=True, help="D:\main\guru.png")
+        #ap.add_argument("-i", "--images", required=True,help="D:\main")
+        #ap.add_argument("-v", "--visualize",help="Flag indicating whether or not to visualize each iteration")
+        #args = vars(ap.parse_args())
         # load the image image, convert it to grayscale, and detect edges
-        template = cv2.imread(args["template"])
+        template = cv2.imread("D:\multi\images\selenium-logo.png")
         template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
         template = cv2.Canny(template, 50, 200)
         (tH, tW) = template.shape[:2]
         cv2.imshow("Template", template)
 
         # loop over the images to find the template in
-        for imagePath in glob.glob(args["images"] + "/*.png"):
+        for imagePath in glob.glob("D:\main" + "/*.png"):
                 # load the image, convert it to grayscale, and initialize the
                 # bookkeeping variable to keep track of the matched region
                 image = cv2.imread(imagePath)
@@ -52,13 +53,13 @@ def Apr():
                         result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
                         (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
                         # check to see if the iteration should be visualized
-                        if args.get("visualize", False):
+                        #if ("visualize", True):
                                 # draw a bounding box around the detected region
-                                clone = np.dstack([edged, edged, edged])
-                                cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
-                                        (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
-                                cv2.imshow("Visualize", clone)
-                                cv2.waitKey(0)
+                                #clone = np.dstack([edged, edged, edged])
+                                #cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
+                                        #(maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+                                #cv2.imshow("Visualize", clone)
+                                #cv2.waitKey(0)
                         # if we have found a new maximum correlation value, then update
                         # the bookkeeping variable
                         if found is None or maxVal > found[0]:
@@ -77,6 +78,6 @@ def Apr():
                 avg=Avg(X,Y)
         return avg
 
-sum=Apr()
-
+def coordinates_click(a,b):
+    pyautogui.click(a,b) 
 
